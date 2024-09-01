ffba385b0c0d4e89f757a35c9fdb94422fb31d24
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-09-01 20:59:33 +0530

    Data types and literals

---
+++ 02.DataTypes and literals.java
@@ -0,0 +1,13 @@
+public class Basic {
+    public static void main(String[] args) {
+        int a = 10; //value assign to variable called literals here literals is 10
+        byte b=12;
+        short c=120;
+        long l=100000;
+        float f=120f;
+        double d=12.5;
+        String s="Hello World";
+        boolean B=true;
+
+    }
+}

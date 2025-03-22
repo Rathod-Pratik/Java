213673360b2e0d7f4a29a6a0c95f44b7767e9f03
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-03-22 13:03:08 +0530

    Create _42_Flowlayout.java

---
+++ _42_Flowlayout.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("FlowLayout");
+        frame.setLayout(new FlowLayout());
+        frame.add(new JButton("Button 1"));
+        frame.add(new JButton("Button 2"));
+        frame.add(new JButton("Button 3"));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}

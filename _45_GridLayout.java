f67f018056bacbc671bf66f707292201285b7c37
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-03-22 17:25:39 +0530

    Create _45_GridLayout.java

---
+++ _45_GridLayout.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class _45_GridLayout {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GridLayout Example");
+        frame.setLayout(new GridLayout(3, 3));
+        for (int i = 1; i <= 9; i++) {
+            frame.add(new JButton("Button " + i));
+        }
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}

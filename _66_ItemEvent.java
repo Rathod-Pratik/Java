20523cf1426efe9989ed2ff77e4f9fe9ff523fb3
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-03-31 11:32:58 +0530

    Create _66_ItemEvent.java

---
+++ _66_ItemEvent.java
@@ -0,0 +1,25 @@
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+public class _66_ItemEvent {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JCheckBox checkBox = new JCheckBox("Accept Terms");
+        checkBox.setBounds(100,100,150,40);
+
+        checkBox.addItemListener(e->{
+            if(e.getStateChange() == e.SELECTED){
+                System.out.println("CheckBox Checked");
+            }
+            else{
+                System.out.println("CheckBox unChecked");
+            }
+        });
+
+        frame.add(checkBox);
+        frame.setSize(300,250);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
